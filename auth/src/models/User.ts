ea3b7f7e02804f24ca00e3598320a24461c789a7
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-18 12:06:36 +0300

    create user model

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,1 +1,20 @@
-//
+import { Document, Schema, model } from 'mongoose';
+
+interface UserDoc extends Document {
+  username: string;
+  email: string;
+  googleId: string;
+}
+
+const userSchema = new Schema<UserDoc>(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    googleId: { type: String, required: true },
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
+
+const User = model<UserDoc>('user', userSchema);
+
+export { User };
