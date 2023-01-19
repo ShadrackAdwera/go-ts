b86248489676179abfed084e769ac826f6a75f2a
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-19 10:55:13 +0300

    create CRUD handlers

--- data/cmd/api/handlers.go
+++ data/cmd/api/handlers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"data/repo"
+	"net/http"
+)
 
 type JsonRequest struct {
 	Title       string `json:"title"`
@@ -8,9 +11,17 @@ type JsonRequest struct {
 }
 
 func (app *Config) GetData(w http.ResponseWriter, r *http.Request) {
+
+	data, err := app.Models.DataEntry.GetData()
+
+	if err != nil {
+		app.errJSON(w, err)
+	}
+
 	response := jsonResponse{
 		Error:   false,
 		Message: "Ping GET data route",
+		Data:    data,
 	}
 
 	app.writeJSON(w, http.StatusCreated, response)
@@ -25,28 +36,53 @@ func (app *Config) PostData(w http.ResponseWriter, r *http.Request) {
 		app.errJSON(w, err)
 		return
 	}
+
+	reqBody := repo.DataEntry{
+		Title:       jsonRequest.Title,
+		Description: jsonRequest.Description,
+	}
+
+	res, err := app.Models.DataEntry.Insert(reqBody)
+
 	response := jsonResponse{
 		Error:   false,
 		Message: "Ping POST data route",
-		Data:    jsonRequest,
+		Data:    res,
 	}
 
 	app.writeJSON(w, http.StatusCreated, response)
 }
 
 func (app *Config) PatchData(w http.ResponseWriter, r *http.Request) {
+	data, err := app.Models.DataEntry.UpdateData()
+
+	if err != nil {
+		app.errJSON(w, err)
+		return
+	}
+
 	response := jsonResponse{
 		Error:   false,
 		Message: "Ping PATCH data route",
+		Data:    data,
 	}
 
 	app.writeJSON(w, http.StatusCreated, response)
 }
 
 func (app *Config) DeleteData(w http.ResponseWriter, r *http.Request) {
+
+	data, err := app.Models.DataEntry.Delete()
+
+	if err != nil {
+		app.errJSON(w, err)
+		return
+	}
+
 	response := jsonResponse{
 		Error:   false,
 		Message: "Ping DELETE data route",
+		Data:    data,
 	}
 
 	app.writeJSON(w, http.StatusCreated, response)
