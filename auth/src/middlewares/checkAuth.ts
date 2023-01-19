38242aeea056d482f76cdd8d303a56a0550d5639
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-19 10:05:19 +0300

    configure auth middleware

--- auth/src/middlewares/checkAuth.ts
+++ auth/src/middlewares/checkAuth.ts
@@ -1,1 +1,10 @@
-//
+import { Request, Response, NextFunction } from 'express';
+
+const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ error: 'You must log in!' });
+  }
+  next();
+};
+
+export default checkAuth;
