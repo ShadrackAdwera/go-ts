6271d45ad30bfb8bc31d34c6d2e64ded032f2743
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-19 10:05:34 +0300

    set up auth routes

--- auth/src/routes/user-routes.ts
+++ auth/src/routes/user-routes.ts
@@ -1,1 +1,24 @@
-//
+import { checkAuth } from '@adwesh/common';
+import { Router } from 'express';
+import passport from 'passport';
+
+import {
+  currentUser,
+  fetchUsers,
+  getAuthCallback,
+  logOut,
+} from './../controllers/user-controllers';
+
+const router = Router();
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google'),
+  getAuthCallback
+);
+router.use(checkAuth);
+router.get('/current-user', currentUser);
+router.get('/users', fetchUsers);
+router.get('/logout', logOut);
+
+export { router as authRoutes };
