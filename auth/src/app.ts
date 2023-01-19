14289ddf9501aeb8944d1092ad71b5eb0d91c3cf
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-19 09:52:38 +0300

    set up stub express connection

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,1 +1,23 @@
-//
+import { HttpError } from '@adwesh/common';
+import express, { Request, Response, NextFunction } from 'express';
+
+import { authRoutes } from './routes/user-routes';
+
+const app = express();
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  throw new HttpError('Invalid Method / Route', 404);
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) return next(error);
+  res.status(error instanceof HttpError ? error.code : 500).json({
+    message:
+      error instanceof HttpError ? error.message : 'Internal Server Error',
+  });
+});
+
+export { app };
