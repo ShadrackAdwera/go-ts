3f03106bddff39929bd2c95216983bbbc04e34ed
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-19 10:46:28 +0300

    configure connection to mongo

--- data/cmd/api/main.go
+++ data/cmd/api/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"context"
+	"data/repo"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const webPort = "5001"
+const (
+	webPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+)
 
-type Config struct{}
+type Config struct {
+	Models repo.Models
+}
 
 func main() {
-	app := Config{}
+
+	client, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+
+	defer cancel()
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: repo.New(client),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -20,10 +50,30 @@ func main() {
 
 	log.Printf("Starting DATA service on port %s\n", webPort)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		log.Panicln(err)
 		return
 	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	log.Println("Connected to mongo!")
+
+	return c, nil
 }
