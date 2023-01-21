cce5190c30d59f0d4a69c74c48f77bb2a44ffd90
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-21 21:59:14 +0300

    modify google client id, google client secret

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -13,19 +13,26 @@ if (!process.env.REDIS_HOST) {
   throw new Error('REDIS HOST is not defined!');
 }
 
-if (!process.env.GOOGLE_CLIENT_ID) {
-  throw new Error('GOOGLE_CLIENT_ID must be defined');
+if (!process.env.GOTS_CLIENT_ID) {
+  throw new Error('GOTS_CLIENT_ID must be defined');
 }
-if (!process.env.GOOGLE_CLIENT_SECRET) {
-  throw new Error('GOOGLE_CLIENT_SECRET must be defined');
+if (!process.env.GOTS_CLIENT_SECRET) {
+  throw new Error('GOTS_CLIENT_SECRET must be defined');
 }
 
 const startApp = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI!, {
+      auth: {
+        username: 'admin',
+        password: 'password',
+      },
+      dbName: 'auth',
+    });
     app.listen(5000);
     console.log('Auth Service Started on PORT: 5000');
   } catch (error) {
+    console.log(error instanceof Error ? error.message : error);
     throw error;
   }
 };
