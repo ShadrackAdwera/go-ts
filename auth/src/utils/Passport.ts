f680e64c7e305eb17ab0a23c88d341e8abbf0499
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-24 15:18:49 +0300

    send auth data to queue on successful login

--- auth/src/utils/Passport.ts
+++ auth/src/utils/Passport.ts
@@ -3,7 +3,8 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 import { User, UserDoc } from './../models/User';
-import { BASE_URL } from './constants';
+import { BASE_URL, COOKIE_MAX_AGE } from './constants';
+import { AmqpClient } from './initAmqp';
 
 passport.serializeUser((user: any, done) => {
   done(undefined, user);
@@ -28,7 +29,10 @@ passport.use(
       try {
         existingUser = await User.findOne({ googleId: profile.id });
         if (existingUser) {
-          // send to RabbitMQ
+          new AmqpClient().sendToQueue({
+            userId: existingUser._id.toString(),
+            expiry: COOKIE_MAX_AGE / 1000,
+          });
           return done(undefined, existingUser);
         }
         const user = await new User({
@@ -39,6 +43,10 @@ passport.use(
               ? profile.emails.map((e) => e.value).toString()
               : '',
         }).save();
+        new AmqpClient().sendToQueue({
+          userId: user._id.toString(),
+          expiry: COOKIE_MAX_AGE / 1000,
+        });
         // send to RabbitMQ
         done(null, user);
       } catch (err) {
